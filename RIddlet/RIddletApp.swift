7421e002b7baa6eae90416b014d20c23830b6434
Alison Eyo <alisonudeme99@gmail.com>
2026-05-06 20:37:22 +0100

    Initial Commit

---
+++ RIddlet/RIddletApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIddletApp.swift
+//  RIddlet
+//
+//  Created by Alison Eyo on 06/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RIddletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
